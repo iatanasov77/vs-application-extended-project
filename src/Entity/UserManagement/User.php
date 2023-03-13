95f9cb536fe0b6359564c6c0c9f2891f113440c5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-03-13 20:05:46 +0000

    Add Extensions Entities.

--- src/Entity/UserManagement/User.php
+++ src/Entity/UserManagement/User.php
@@ -2,13 +2,27 @@
 
 use Doctrine\ORM\Mapping as ORM;
 use Vankosoft\UsersBundle\Model\User as BaseUser;
+use Vankosoft\UsersSubscriptionsBundle\Model\Interfaces\SubscribedUserInterface;
+use Vankosoft\UsersSubscriptionsBundle\Model\Traits\SubscribedUserTrait;
+use Vankosoft\PaymentBundle\Model\Interfaces\PaymentsUserInterface;
+use Vankosoft\PaymentBundle\Model\Traits\PaymentsUserTrait;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="VSUM_Users")
  */
-class User extends BaseUser
+class User extends BaseUser implements SubscribedUserInterface, PaymentsUserInterface
 {
+    use SubscribedUserTrait;
+    use PaymentsUserTrait;
+    
+    public function __construct()
+    {
+        $this->subscriptions    = new ArrayCollection();
+        $this->orders           = new ArrayCollection();
+        
+        parent::__construct();
+    }
     /**
      * {@inheritDoc}
      */
