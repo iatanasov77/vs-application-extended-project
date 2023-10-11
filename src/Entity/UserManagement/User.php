deb5e3cd26352c45ee698be243566b6c5e5657c6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-10-11 19:12:00 +0000

    Some Updates .

--- src/Entity/UserManagement/User.php
+++ src/Entity/UserManagement/User.php
@@ -6,25 +6,27 @@ use Doctrine\Common\Collections\Collection;
 use Vankosoft\UsersBundle\Model\User as BaseUser;
 use Vankosoft\UsersSubscriptionsBundle\Model\Interfaces\SubscribedUserInterface;
 use Vankosoft\UsersSubscriptionsBundle\Model\Traits\SubscribedUserTrait;
-use Vankosoft\PaymentBundle\Model\Interfaces\PaymentsUserInterface;
-use Vankosoft\PaymentBundle\Model\Traits\PaymentsUserTrait;
+use Vankosoft\PaymentBundle\Model\Interfaces\UserPaymentAwareInterface;
+use Vankosoft\PaymentBundle\Model\Traits\UserPaymentAwareTrait;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="VSUM_Users")
  */
-class User extends BaseUser implements SubscribedUserInterface, PaymentsUserInterface
+class User extends BaseUser implements SubscribedUserInterface, UserPaymentAwareInterface
 {
     use SubscribedUserTrait;
-    use PaymentsUserTrait;
+    use UserPaymentAwareTrait;
     
     public function __construct()
     {
+        parent::__construct();
+        
         $this->subscriptions            = new ArrayCollection();
+        
+        $this->paymentDetails           = [];
         $this->orders                   = new ArrayCollection();
         $this->pricingPlanSubscriptions = new ArrayCollection();
-        
-        parent::__construct();
     }
     /**
      * {@inheritDoc}
